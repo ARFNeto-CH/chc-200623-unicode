4210ecf8c281e81871e14572834d9109d3f56fdf
ARFNeto-CH <arfneto@hotmail.com>
2020-06-24 01:20:12 -0300

    uniode na console C trecho 2b00

---
+++ outrocsv.c
@@ -0,0 +1,42 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <fcntl.h> 
+#include <io.h>
+#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <wchar.h>
+#include <windows.h>
+
+int main(int argc, char** argv)
+{
+    const int Unicode = _O_U16TEXT;
+    const int Non_Unicode = _O_TEXT;
+    int console = _fileno(stdout);
+    if (console < 0)
+    {
+        fprintf(stderr, "saida provavelmente rediredirecionada\n");
+        return -1;
+    };  // if()
+    SetConsoleOutputCP(65'001);
+    wchar_t pagina[160];
+    for (int i = 0; i < 160; i += 1)
+    {
+        pagina[i] = 0x2b00 + i;
+    };
+    HANDLE CON = GetStdHandle(STD_OUTPUT_HANDLE);
+    _setmode(console, Unicode);
+    for (int i = 0; i < 160; i += 1)
+    {
+        wprintf(L"%4X%4c  ", pagina[i], pagina[i]);
+        if ((1+i)% 8 == 0) wprintf(L"\n");
+    };
+    wprintf(L"\n");
+    _setmode(console, Non_Unicode);
+    printf("printf() normal\n");
+
+    return 0;
+};
+
+//  fim
